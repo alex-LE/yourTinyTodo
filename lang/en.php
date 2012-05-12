38afef147143892da093cfb89f6606b034823fab
alex-LE <info@alexander-adam.net>
2012-05-12 17:32:30 +0200

    added some text to lang files -> issue #30

--- lang/en.php
+++ lang/en.php
@@ -164,6 +164,6 @@ class Lang extends DefaultLang
 		'um_rolename_3' => "read only",
 		'um_nousers' => "No users available",
 		'um_createuser' => "Create user",
-
+		'access_denied' => "Access denied!<br/>Disable password protection or Log in.",
 	);
 }
