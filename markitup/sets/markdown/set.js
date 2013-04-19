32be582a38991b65c1ba23b26b4a343f4cad2090
alex-LE <info@alexander-adam.net>
2013-04-19 20:12:42 +0200

    added new files for markdown

---
+++ markitup/sets/markdown/set.js
@@ -0,0 +1,76 @@
+// -------------------------------------------------------------------
+// markItUp!
+// -------------------------------------------------------------------
+// Copyright (C) 2008 Jay Salvat
+// http://markitup.jaysalvat.com/
+// -------------------------------------------------------------------
+// MarkDown tags example
+// http://en.wikipedia.org/wiki/Markdown
+// http://daringfireball.net/projects/markdown/
+// -------------------------------------------------------------------
+// Feel free to add more tags
+// -------------------------------------------------------------------
+mySettings = {
+	previewParserPath:	'',
+	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
+	markupSet: [
+		{name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
+		{name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
+		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
+		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
+		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
+		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
+		{separator:'---------------' },
+		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
+		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
+		{separator:'---------------' },
+		{name:'Bulleted List', openWith:'- ' },
+		{name:'Numeric List', openWith:function(markItUp) {
+			return markItUp.line+'. ';
+		}},
+		{separator:'---------------' },
+		{name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
+		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
+		{separator:'---------------'},	
+		{name:'Quotes', openWith:'> '},
+		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
+		{separator:'---------------'},
+		{	name:'Table generator', 
+			className:'tablegenerator', 
+			placeholder:"Your text here...",
+			replaceWith:function(markItUp) {
+				var cols = prompt("How many cols?"),
+					rows = prompt("How many rows?"),
+					html = "<table>\n";
+				if (markItUp.altKey) {
+					html += " <tr>\n";
+					for (var c = 0; c < cols; c++) {
+						html += "! [![TH"+(c+1)+" text:]!]\n";	
+					}
+					html+= " </tr>\n";
+				}
+				for (var r = 0; r < rows; r++) {
+					html+= " <tr>\n";
+					for (var c = 0; c < cols; c++) {
+						html += "  <td>"+(markItUp.placeholder||"")+"</td>\n";	
+					}
+					html+= " </tr>\n";
+				}
+				html += "</table>\n";
+				return html;
+			}
+		}
+	]
+}
+
+// mIu nameSpace to avoid conflict.
+miu = {
+	markdownTitle: function(markItUp, char) {
+		heading = '';
+		n = $.trim(markItUp.selection||markItUp.placeHolder).length;
+		for(i = 0; i < n; i++) {
+			heading += char;
+		}
+		return '\n'+heading;
+	}
+}
